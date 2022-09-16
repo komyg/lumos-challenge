d1b3911734ec427712c096a6f56dd4777923b423
Felipe Armoni <felipe.armoni@gmail.com>
2022-09-16 14:22:00 -0300

    Added extra calls to okta

--- app/okta/infrastructure/okta_client/client.py
+++ app/okta/infrastructure/okta_client/client.py
@@ -8,3 +8,18 @@ def list_users(query_params: str) -> dict[str, any]:
 
     resp = requests.get(url=url, params=query_params, headers=get_base_headers())
     return resp.json()
+
+
+def suspend_user(user_id: str) -> None:
+    url = f"{get_base_url()}/api/v1/users/{user_id}/lifecycle/suspend"
+    requests.post(url=url, headers=get_base_headers())
+
+
+def unsuspend_user(user_id: str) -> None:
+    url = f"{get_base_url()}/api/v1/users/{user_id}/lifecycle/unsuspend"
+    requests.post(url=url, headers=get_base_headers())
+
+
+def delete_user(user_id: str) -> None:
+    url = f"{get_base_url()}/api/v1/users/{user_id}"
+    requests.delete(url=url, headers=get_base_headers())
